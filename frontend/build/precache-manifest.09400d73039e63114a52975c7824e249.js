b60b40b64c39c72c3835a91edddb939ace709415
Edy Gunawan <gunawanedy40@gmail.com>
2020-09-13 16:42:01 +0700

    React

---
+++ frontend/build/precache-manifest.09400d73039e63114a52975c7824e249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e945346e376cb0ee92a9a50d1f8a7acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e335bec3a32d6c4a844",
+    "url": "/static/css/main.3b0825af.chunk.css"
+  },
+  {
+    "revision": "514a8a93332ef3da16f9",
+    "url": "/static/js/2.53e0a205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53e0a205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e335bec3a32d6c4a844",
+    "url": "/static/js/main.eefe075c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
